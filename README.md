87d67aa32ed3633e15a117d23e931ebab069e9c2
Mehdi <mehdilauters@gmail.com>
2017-08-20 21:02:54 +0200

    Working version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # tgvconnect_gpsd_bridge
-a simple gpsd source from onboard french GPS
+a simple gpsd source from onboard french TGV GPS
+![Portal screenshot](img/tgvconnect.png)
+
+## Usage
+
+- connect to the ````_tgv_connect```` wifi
+- start the bridge by running ````python tgvconnect_gpsd_bridge.py````
+- start gpsd as follow ````gpsd -N gpsd://localhost:5006````
+- Connect to gpsd with your favorite client such as ````cgps````
--- tgvconnect_gpsd_bridge.py
+++ tgvconnect_gpsd_bridge.py
@@ -1,11 +1,13 @@
 import socket
 import urllib2
 import json
+import time
+from datetime import datetime
 gps_uri = 'https://www.tgvconnect.com/router/api/train/gps'
 
 
 TCP_IP = ''
-TCP_PORT = 5005
+TCP_PORT = 5006
 BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
 
 #  run this script
@@ -15,11 +17,13 @@ BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
 
 def get_gps_data():
   
+  mode = 1
   try:
     req = urllib2.Request(gps_uri)
     response = urllib2.urlopen(req)
     plain = response.read()
     data = json.loads(plain)
+    mode = 3
   except:
     print "Error fetching gps data"
   
@@ -28,15 +32,24 @@ def get_gps_data():
     #'longitude': -0.1,
     #'altitude': 20,
     #'heading': 230,
-    #'speed': 230
+    #'speed': 10
     #}
   
-  res='''{"class":"TPV","device":"/dev/pts/1",
-    "time":"2005-06-08T10:34:48.283Z","ept":0.005,
-    "lat":%s,"lon":%s,"alt":%s,
-    "eph":36.000,"epv":32.321,
-    "track":%s,"speed":%s,"climb":-0.085,"mode":3}'''%(data['latitude'], data['longitude'], data['altitude'], data['heading'], data['speed'])
+  hour = datetime.now().isoformat()
+  res='''{"class":"TPV","device":"/dev/ttyUSB0","mode":%s,"time":"%s","ept":0.005,"lat":%s,"lon":%s,"alt":%s,"epx":9.125,"epy":17.778,"epv":34.134,"track":%s,"speed":%s,"climb":0.000,"eps":36.61}'''%(mode, hour, data['latitude'], data['longitude'], data['altitude'], data['heading'], data['speed'])
   return res
+
+def send_loop(conn):
+  while 1:
+    #data = conn.recv(BUFFER_SIZE)
+    #if not data: break
+    #print "received data:", data
+    tosend = get_gps_data()
+    print tosend
+    conn.send(tosend)  # echo
+    time.sleep(1)
+  print "close"
+  conn.close()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
@@ -44,12 +57,4 @@ s.listen(1)
 
 conn, addr = s.accept()
 print 'Connection address:', addr
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print "received data:", data
-    tosend = get_gps_data()
-    print tosend
-    conn.send(tosend)  # echo
-print "close"
-conn.close()
+send_loop(conn)
